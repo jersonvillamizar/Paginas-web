4dca424013fa19cf4b4b564783637c73d6f66617
jersonvillamizar <archi.javf@gmail.com>
2023-10-05 20:54:04 -0500

    Biblioteca ingreso

---
+++ javascript_new/05_10_2023/funcion.js
@@ -0,0 +1,11 @@
+let ingreso_portada_url = document.getElementById('ingreso_portada_url').value;
+let ingreso_titulo = document.getElementById('ingreso_titulo').value;
+let ingreso_autor = document.getElementById('ingreso_autor').value;
+let ingreso_año = document.getElementById('ingreso_año').value;
+let ingreso_disponible = document.getElementById('ingreso_disponible').value;
+let ingreso_prestado = document.getElementById('ingreso_prestado').value;
+
+let dicc = {}
+const list = []
+
+
